f67728543383ef7a9b03bbb043488e7ebd892ee5
fo-xi <60314037+fo-xi@users.noreply.github.com>
2020-03-15 23:03:53 +0700

    feature(Сommon): изменена функция ReadNumber

--- LaboratoryWork1/Common/ReadNumber.h
+++ LaboratoryWork1/Common/ReadNumber.h
@@ -1,3 +1,24 @@
 #pragma once
+#include <iostream>
 
-int ReadNumber();
+using namespace std;
+
+template <typename T>
+T ReadNumber()
+{
+	bool error;
+	T number;
+	do
+	{
+		error = false;
+		cin >> number;
+		if (cin.fail())
+		{
+			cout << "Error\n";
+			error = true;
+			cin.clear();
+			cin.ignore(80, '\n');
+		}
+	} while (error);
+	return number;
+}
