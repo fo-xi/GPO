653e33b62c0c2a61a09c580ee8fff020cca47f8d
alexey.kalentyev <alexey.kalentyev@50ohm.tech>
2020-05-21 19:04:02 +0700

    Провверил, принял

--- LaboratoryWork1/LaboratoryWork3/ReadValueRange.h
+++ LaboratoryWork1/LaboratoryWork3/ReadValueRange.h
@@ -2,7 +2,7 @@
 #include <iostream>
 
 using namespace std;
-//TODO: Очень негибко получилось... Гибче бы было передавать границы в качестве входных параметров. Поэтому и дублируется с 0-10 (+)
+
 template <typename T>
 T ReadValueRange(T number, T initialBorder, T finalBorder)
 {
